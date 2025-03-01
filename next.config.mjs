0e43ffcb5b50475ddede2f394e7b79b9b4d24bf2
Konstantin Yurchenko, Jr. <konstantin.k.yurchenko@gmail.com>
2025-02-28 18:59:45 -0800

    Fixes issue with listing videos.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "skateconnect.s3.us-west-2.amazonaws.com",
+            pathname: "/**",
+          },
+        ],
+    },
 };
 
 export default nextConfig;
